81430c6a2552df90fe0f5468abad3635f4c9e82b
Difta Fitrahul Qihaj <88217314+DiftaFitrahul@users.noreply.github.com>
2022-11-13 20:05:48 +0700

    Install React Router
    Menginstall react router ke dalam project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
+import  {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import AboutPage from './AboutPage.js';
 import Navbar from './Navbar.js';
 
+
 function App() {
   return (
-    <div className="App">
+    <Router>
+      <div className="App">
       <Navbar />
-    </div>
+      </div>
+    </Router>
+    
   );
 }
 
