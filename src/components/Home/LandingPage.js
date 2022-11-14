8e96d289945f9d8c152ec8d7647b6acda77d1694
Daffa K <daffa.kamal@mail.ugm.ac.id>
2022-11-14 20:56:33 +0700

    feat(Home): add ChevronDownIcon

--- src/components/Home/LandingPage.js
+++ src/components/Home/LandingPage.js
@@ -1,8 +1,10 @@
+import { ChevronDownIcon } from '@heroicons/react/24/solid';
 import LogoNL from '../../assets/images/Home/Logo NL.png';
+
 
 const LandingPage = () => {
     return (
-        <div className="w-full h-screen grid justify-items-center content-center">
+        <div className="w-full h-screen relative grid justify-items-center content-center">
             <img
                 src={LogoNL}
                 className="w-[300px]"
@@ -11,6 +13,9 @@ const LandingPage = () => {
             <h1 className="font-poppinsBold text-center text-white text-[55px] pt-[10px]">
                 Night Login UGM
             </h1>
+            <a className="absolute mx-auto bottom-8 cursor-pointer">
+                <ChevronDownIcon className="w-12 lg:w-14 text-white" />
+            </a>
         </div>
     );
 }
