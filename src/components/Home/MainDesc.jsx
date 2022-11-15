b9a88b9033d835ce7720cf1db52375a7395e1556
Daffa K <daffa.kamal@mail.ugm.ac.id>
2022-11-15 13:45:13 +0700

    fix(Home): applying href attribute; add scroll-behaviour

--- src/components/Home/MainDesc.jsx
+++ src/components/Home/MainDesc.jsx
@@ -7,7 +7,10 @@ const MainDesc = () => {
     const [list] = useState(komunitasList);
 
     return (
-        <div className="w-full bg-pink-NL relative grid justify-items-center content-center rounded-5xl mt-10">
+        <div 
+            id={"MainDesc"}
+            className="w-full bg-pink-NL relative grid justify-items-center content-center rounded-5xl mt-10"
+        >
             <h1 className="font-poppinsBold text-black-NL text-[55px] py-[90px]">
                 #BeginToGain
             </h1>
