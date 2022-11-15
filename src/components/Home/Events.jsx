1e60b2649df47796d18224f9244c69bbb44b13aa
Daffa K <daffa.kamal@mail.ugm.ac.id>
2022-11-15 20:44:31 +0700

    fix(Home): fixing bg-color on LandingPage section

--- src/components/Home/Events.jsx
+++ src/components/Home/Events.jsx
@@ -2,20 +2,22 @@ const styleButton = "items-center px-[26px] py-[3px] rounded-[58px] bg-white-NL 
 
 const Events = () => {
     return (
-        <div className="w-full bg-red-NL relative grid justify-items-center content-center rounded-5xl pb-48">
-            <h1 className="font-poppinsBold text-white-NL text-[40px] pt-[161px] pb-[70px]">
-                Our Events
-            </h1>
-            <div className="flex flex-row gap-x-[48px] justify-center">
-                <button className={styleButton}>
-                    2020
-                </button>
-                <button className={styleButton}>
-                    2021
-                </button>
-                <button className={styleButton}>
-                    2022
-                </button>
+        <div className="w-full bg-white-NL">
+            <div className="h-full relative grid justify-items-center content-center bg-red-NL rounded-5xl pb-48">
+                <h1 className="font-poppinsBold text-white-NL text-[40px] pt-[130px] pb-[70px]">
+                    Our Events
+                </h1>
+                <div className="flex flex-row gap-x-[48px] justify-center">
+                    <button className={styleButton}>
+                        2020
+                    </button>
+                    <button className={styleButton}>
+                        2021
+                    </button>
+                    <button className={styleButton}>
+                        2022
+                    </button>
+                </div>
             </div>
         </div>
     );
