1a0b23da086573e812b916f341b27e50d4b4fa27
Daffa K <daffa.kamal@mail.ugm.ac.id>
2022-11-16 21:35:31 +0700

    fix(Layout): Footer responsiveness

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -11,7 +11,7 @@ const Footer = () => {
     const footerButtons = (title, link) => {
         return (
             <Link 
-                className="font-poppinsRegular text-[20px]" 
+                className="font-poppinsRegular text-base xl:text-[20px]" 
                 to={link}
             >
                 {title}
@@ -23,18 +23,18 @@ const Footer = () => {
         <div className="w-full h-full bg-black-NL px-[135px] py-[70px] z-40">
             <div className="flex items-center text-white-NL gap-x-6">
                 <img
-                    className="w-[51px]" 
+                    className="w-[45px] xl:w-[51px]" 
                     src={logoNL} 
                     loading="lazy"
                     alt="Logo Night Login"
                 />
-                <h1 className="font-poppinsBold text-[30px]">
+                <h1 className="font-poppinsBold text-[26px] xl:text-[30px]">
                     Night Login
                 </h1>
             </div>
             <div className="flex flex-row justify-between text-white-NL">
-                <div className="grid max-w-lg">
-                    <p className="font-poppinsLight text-[18px] py-5">
+                <div className="grid max-w-xs xl:max-w-md 2xl:max-w-lg">
+                    <p className="font-poppinsLight text-base xl:text-[18px] py-5">
                         Lorem ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since.
                     </p>
                     <div className="flex gap-x-4">
@@ -46,7 +46,7 @@ const Footer = () => {
                             >
                                 <img
                                     src={item.src}
-                                    className="w-[26px]"
+                                    className="w-[23px] xl:w-[26px]"
                                     loading="lazy"
                                     alt={`${item.title} icon`}
                                 />
@@ -54,9 +54,9 @@ const Footer = () => {
                         ))}
                     </div>
                 </div>
-                <div className="flex gap-x-48">
+                <div className="flex gap-x-32 2xl:gap-x-48">
                     <div className="grid">
-                        <p className="font-poppinsMedium text-[20px]">
+                        <p className="font-poppinsMedium text-base xl:text-[20px]">
                             Quick Links :
                             <div className="border-red-NL border-2"></div>
                         </p>
@@ -65,13 +65,13 @@ const Footer = () => {
                         {footerButtons ("Community", "#!")}
                     </div>
                     <div className="grid">
-                        <p className="font-poppinsMedium text-[20px] pb-4">
+                        <p className="font-poppinsMedium text-base xl:text-[20px] pb-4">
                             Lorem ipsum :
                             <div className="border-red-NL border-2"></div>
                         </p>
                         <img
                             src={logoKMTETI}
-                            className="w-[94px]"
+                            className="w-[86px] xl:w-[94px]"
                             loading="lazy"
                             alt="Logo KMTETI"
                         />
