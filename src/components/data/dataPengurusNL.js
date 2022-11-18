79c4a0805151792ee5e9ef4d86be1bb810085306
Difta Fitrahul Qihaj <88217314+DiftaFitrahul@users.noreply.github.com>
2022-11-18 16:41:01 +0700

    feat(Data): add data NL team
    Membuat objek js yang berisi profil dari semua pengurus Ninght Login

---
+++ src/components/data/dataPengurusNL.js
@@ -0,0 +1,51 @@
+const PengurusNL = {
+    leader: {
+        president: "Samatha Marhaendra P.",
+        vice: "Christian Ale P.",
+    },
+    managementTeam: {
+        publicRealtionTeam: {
+            head: "Sagara Aldavy M.N.S",
+            members: ["Khairun Nisa’ Z.", "Byan Cahaya R. A"]
+        },
+        creativeMediaTeam: {
+            head: "Arifah Amelia T.",
+            members: ["Fajar Rizki W.","M. Daniyal Kautsar"]
+        },
+        administrationTeam: {
+            head: "Gerrit Ezra Yudi K.",
+            members: ["Talitha Andani","Dhiyaa Amalia P","Anisa Nur Rahmalina"]
+        }
+        
+    },
+    communityTeam: {
+        nadc: {
+            head: "Firdaus Bisma Suryakusuma",
+            viceHead: "Muhammad Hasan Chamdany"
+        },
+        ncpc: {
+            head: "Harry Krisna Darmawan",
+            viceHead: "Wiweka Yoga Sadewa"
+        },
+        nddc: {
+            head: "Rizky Alif Ramadhan",
+            viceHead: "Karunia Perjuangan"
+        },
+        nlnc: {
+            head: "Muhammad Farrel Rafirizqy",
+            viceHead: "Novaldy Pratama"
+        },
+        nuxc: {
+            head: "Tiara Kesuma Putri",
+            viceHead: "Annisa Somaningtyas"
+        },
+        nwdc: {
+            head: "Aldo Arya Saka Mukti",
+            viceHead: "Alphonsus Jovian Joy Reynaldo"
+        },
+        
+    }
+
+
+
+}
