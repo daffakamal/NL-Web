ec0e1a58423e2050fefb955d2ff2caffbae64793
Daffa K <daffa.kamal@mail.ugm.ac.id>
2022-11-18 21:12:13 +0700

    fix(Home): fixing pb

--- src/components/Home/MainDesc.jsx
+++ src/components/Home/MainDesc.jsx
@@ -14,7 +14,7 @@ const MainDesc = () => {
             <h1 className="font-poppinsBold text-black-NL text-[55px] py-[90px]">
                 #BeginToGain
             </h1>
-            <div className="w-full bg-white-NL relative rounded-5xl md:px-20 lg:px-[134px]">
+            <div className="w-full bg-white-NL relative rounded-5xl md:px-20 lg:px-[134px] pb-12 lg:pb-28">
                 <div className="grid justify-items-center content-center mx-auto container">
                     <h1 className="font-poppinsBold text-red-NL text-[40px] pt-[90px] pb-[70px]">
                         About Night Login
