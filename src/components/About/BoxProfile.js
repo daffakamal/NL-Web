7a983a02bc129ec85dcaafcdd0dc70583ff4afdf
Daffa K <dvffvkml@gmail.com>
2023-03-27 22:53:29 +0700

    fix(About): update font size in BoxProfile

--- src/components/About/BoxProfile.js
+++ src/components/About/BoxProfile.js
@@ -2,8 +2,12 @@ const BoxProfile = ({title, name}) => {
     return (
       <div className="w-fit flex flex-col justify-center my-6 mx-12 xl:mx-16">
         <div className="mx-auto w-48 sm:w-56 lg:w-60 2xl:w-72 h-[210px] sm:h-[230px] lg:h-64 2xl:h-76 bg-grey-NL rounded-4xl border-solid border-black border-4 shadow-custom2"></div>
-        <p className="font-poppinsSemi mt-8 text-white text-2xl">{title}</p>
-        <p className="font-poppinsRegular my-2 text-white text-xl">{name}</p>
+        <p className="font-poppinsSemi mt-8 text-white text-[18px] md:text-xl lg:text-2xl">
+          {title}
+        </p>
+        <p className="font-poppinsRegular my-2 text-white text-[16px] md:text-[18px] lg:text-xl">
+          {name}
+        </p>
       </div>
     );
 }
